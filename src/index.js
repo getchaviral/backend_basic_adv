c128c0bcc4f57694da505ada12f2ab1c34d52453
getchaviral <shuklaavrial403@gmail.com>
2025-03-15 11:44:39 +0530

    testing a ccommit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import  {DB_NAME} from "./constants"
+
+import express from "express";
+
+(async ()=>{
+    try {
+        mongoose.connect('${process.env.MONGODB_URI}/${ DB_NAME}')
+     }
+     catch (error) {
+        console.error("ERROR",error);
+        }
+
+})()
